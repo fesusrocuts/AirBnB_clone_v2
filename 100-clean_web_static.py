34ee8fe2ff67f5a3fd5294cbb13640423af3f3b0
fesus rocuts <fesusrocuts@gmail.com>
2019-08-23 23:44:22 -0500

    t4 c3

--- 100-clean_web_static.py
+++ 100-clean_web_static.py
@@ -7,45 +7,62 @@ from datetime import datetime
 from os.path import exists, isdir
 env.hosts = ['104.196.127.61', '35.229.87.105']
 
-@runs_once
-def clean_local(number=0):
-    """
-    remove local versions step clean_server
-    number: files to keep
-    """
-    # change directory and list files
-    os.chdir("versions/")
-    res = local("ls -1t", capture=True).split("\n")
-    if number == 0:
-        number += 1
-    if len(res) > number:
-        for f in res[number:]:
-            local("rm -f {}".format(f))
+def do_pack():
+    ts = time.strftime("%Y%m%d%H%M%S")
+    try:
+        local("mkdir -p versions")
+        local("tar -cvzf versions/web_static_{}.tgz web_static/".
+              format(ts))
+        return ("versions/web_static_{}.tgz".format(ts))
+    except:
+        return None
 
 
-def clean_server(number=0):
+def do_deploy(archive_path):
     """
-    remove versions step clean_server
-    number: files to keep
+    deploy
     """
-    path = "/data/web_static/releases"
-    with cd(path):
-        res = sudo("ls -1t").split("\r\n")
-    res = [f for f in res if "web_static" in f]
-    if number == 0:
-        number += 1
-    if len(res) > number:
-        res = res[number:]
-        with cd(path):
-            for f in res:
-                sudo("rm -rf {}".format(f))
+    if (os.path.isfile(archive_path) is False):
+        return False
+
+    try:
+        new_comp = archive_path.split("/")[-1]
+        new_folder = ("/data/web_static/releases/" + new_comp.split(".")[0])
+        put(archive_path, "/tmp/")
+        run("sudo mkdir -p {}".format(new_folder))
+        run("sudo tar -xzf /tmp/{} -C {}".
+            format(new_comp, new_folder))
+        run("sudo rm /tmp/{}".format(new_comp))
+        run("sudo mv {}/web_static/* {}/".format(new_folder, new_folder))
+        run("sudo rm -rf {}/web_static".format(new_folder))
+        run('sudo rm -rf /data/web_static/current')
+        run("sudo ln -s {} /data/web_static/current".format(new_folder))
+        return True
+    except:
+        return False
+
+
+def deploy():
+    try:
+        archive_address = do_pack()
+        val = do_deploy(archive_address)
+        return val
+    except:
+        return False
 
 
 def do_clean(number=0):
-    """
-    remove old version step do_clean
-    number: files to keep
-    """
-    number = int(number)
-    clean_local(number)
-    clean_server(number)
+    if number == 0 or number == 1:
+        with cd.local('./versions/'):
+            local("ls -lv | rev | cut -f 1 | rev | \
+            head -n +1 | xargs -d '\n' rm -rf")
+        with cd('/data/web_static/releases/'):
+            run("sudo ls -lv | rev | cut -f 1 | \
+            rev | head -n +1 | xargs -d '\n' rm -rf")
+    else:
+        with cd.local('./versions/'):
+            local("ls -lv | rev | cut -f 1 | rev | \
+            head -n +{} | xargs -d '\n' rm -rf".format(number))
+        with cd('/data/web_static/releases/'):
+            run("sudo ls -lv | rev | cut -f 1 | \
+            rev | head -n +{} | xargs -d '\n' rm -rf".format(number))
